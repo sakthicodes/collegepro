e03e3073580654d531229507b1c48e1141d1f6b7
sakthicodes <sakthicodedev@gmail.com>
2024-03-31 22:35:40 +0530

    first commit

---
+++ SMP/index.php
@@ -0,0 +1,73 @@
+<?php require("./layout/Header.php") ?>
+<?php require("./layout/db.php") ?>
+
+<div class="container-fluid py-4">
+    <div class="row">
+        
+    <div class="col-sm-4 mb-3 mb-sm-0 r-8">
+        <div class="card">
+          <div class="card-body p-2">
+            <p class="card-title">Special title treatment</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+</div>
+<div class="row">
+      <div class="col-sm-4 justify-content-end mb-3">
+        <div class="card" >
+          <div class="card-body p-2">
+          <p class="card-title text-end">Special title treatment</p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a href="#" class="btn btn-primary me-md-2">Go somewhere</a>
+            </div>         
+         </div>
+        </div>
+</div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body p-2">
+          <p class="card-title">Special title treatment</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4 mb-3">
+        <div class="card">
+          <div class="card-body p-2">
+          <p class="card-title text-end">Special title treatment</p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <a href="#" class="btn btn-primary me-md-2">Go somewhere</a>
+            </div>          
+        </div>
+        </div>
+      </div>
+      <div class="col-sm-12 mb-3">
+        <div class="card">
+          <div class="card-body p-3">
+          <p class="card-title text-center">Special title treatment</p>
+          <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+            <a href="#" class="btn btn-primary me-md-2">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 mb-3">
+        <div class="card">
+          <div class="card-body p-6">
+          <p class="card-title text-end">Special title treatment</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+       
+        
+       
+    </div>
+
+</div>
+
+
+<?php require("./layout/Footer.php") ?>
---
+++ SMP/layout/Header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="/assets/img/favicon.png">
+  <title>
+    SJCET -TNJ
+  </title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <link id="pagestyle" href="./assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+</head>
